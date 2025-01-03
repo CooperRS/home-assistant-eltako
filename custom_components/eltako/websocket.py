421b2ad9ee166897350dd3fe849b2a5e538f24dc
Philipp Grimm <grimm.philipp@gmail.com>
2025-01-03 09:32:45 +0100

    ws info extended

--- custom_components/eltako/websocket.py
+++ custom_components/eltako/websocket.py
@@ -11,6 +11,15 @@ async def register_websockets(hass: HomeAssistant, config: ConfigEntry):
     websocket_api.async_register_command(hass, ws_info)
 
 
+def _get_manifest_info():
+
+    try:
+        with open("manifest.json", "r") as file:
+            response = json.load(file)
+            logging.getLogger().info(f"info response: {response}")
+    except:
+        response = {}
+
 @websocket_api.websocket_command({
     'type': 'eltako/info',
     'required': []
@@ -18,13 +27,7 @@ async def register_websockets(hass: HomeAssistant, config: ConfigEntry):
 @websocket_api.async_response
 async def ws_info(hass: HomeAssistant, connection, msg):
     
-    try:
-        with open("manifest.json", "r") as file:
-            response = json.load(file)
-            logging.getLogger().info(f"info response: {response}")
-    except:
-        response = {}
-    
+    response = await hass.async_add_executor_job(_get_manifest_info)
 
     # Send the response back
     connection.send_message(websocket_api.result_message(msg['id'], response))
