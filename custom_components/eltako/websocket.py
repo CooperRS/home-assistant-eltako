10aa1994e66149eebd7b81764611dffc03f6bdd2
Philipp Grimm <grimm.philipp@gmail.com>
2025-01-07 23:01:50 +0100

    bug fix in binary sensor

--- custom_components/eltako/websocket.py
+++ custom_components/eltako/websocket.py
@@ -2,7 +2,6 @@ import json
 import os
 from .const import *
 
-from homeassistant.components import websocket_api
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.components import websocket_api
@@ -17,7 +16,7 @@ def _get_manifest_info():
         dir_path = os.path.dirname(__file__)
         with open(os.path.join(dir_path, "manifest.json"), "r") as file:
             response = json.load(file)
-            LOGGER.info(f"info response: {response}")
+            # LOGGER.info(f"info response: {response}")
     except Exception as e:
         LOGGER.error("Cannot read manifest.json", exc_info=True, stack_info=True)
         response = {}
@@ -32,11 +31,9 @@ def _get_manifest_info():
 @websocket_api.async_response
 async def ws_info(hass: HomeAssistant, connection, msg):
     
-    LOGGER.debug("Call WS eltako/info")
+    # LOGGER.debug("Call WS eltako/info")
 
     response = await hass.async_add_executor_job(_get_manifest_info)
-
-    LOGGER.debug(f"version {response['version']}")
 
     # Send the response back
     connection.send_message(websocket_api.result_message(msg['id'], response))
