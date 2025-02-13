a50d1453cdc1c64a1e995370a5fc614922fe2f04
Philipp Grimm <grimm.philipp@gmail.com>
2025-02-13 13:54:58 +0100

    test frontend lib

--- custom_components/eltako/eltako_integration_init.py
+++ custom_components/eltako/eltako_integration_init.py
@@ -38,11 +38,11 @@ async def async_setup(hass: HomeAssistant, config_type: ConfigType) -> bool:
 
     # Read the config
     LOGGER.debug(f"[{LOG_PREFIX_INIT}] Load Config")
-    # config = await config_helpers.async_get_home_assistant_config(hass, CONFIG_SCHEMA)
-    # LOGGER.debug(f"[{LOG_PREFIX_INIT}] Config: {config}")
-    # hass.data[DATA_ELTAKO] = hass.data.setdefault(DATA_ELTAKO, {})
-    # hass.data[DATA_ELTAKO][ELTAKO_CONFIG] = await config_helpers.async_get_home_assistant_config(hass, CONFIG_SCHEMA)
-    # general_settings = config_helpers.get_general_settings_from_configuration(hass)
+    config = await config_helpers.async_get_home_assistant_config(hass, CONFIG_SCHEMA)
+    LOGGER.debug(f"[{LOG_PREFIX_INIT}] Config: {config}")
+    hass.data[DATA_ELTAKO] = hass.data.setdefault(DATA_ELTAKO, {})
+    hass.data[DATA_ELTAKO][ELTAKO_CONFIG] = config
+    general_settings = config_helpers.get_general_settings_from_configuration(hass)
 
     # if general_settings[CONF_FRONTEND_ENABLED] == 'False': return
 
@@ -85,10 +85,12 @@ async def async_setup(hass: HomeAssistant, config_type: ConfigType) -> bool:
         )
 
     else:
+        local_path = eltako_frontend.locate_dir()+"/static"
+        LOGGER.debug(f"[{LOG_PREFIX_INIT}] Load static path from library {local_path}")
         # Include frontend from library
         StaticPathConfig(
             "home-assistant-eltako-frontend",
-            path=eltako_frontend.locate_dir()+"/static",
+            path=local_path,
             cache_headers=False
         )
 
