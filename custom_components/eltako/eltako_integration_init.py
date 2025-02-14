32c4cd6b6960136e9ca8389bc69ba6cd5df64b5f
Philipp Grimm <grimm.philipp@gmail.com>
2025-02-15 00:30:39 +0100

    test frontend lib

--- custom_components/eltako/eltako_integration_init.py
+++ custom_components/eltako/eltako_integration_init.py
@@ -110,7 +110,7 @@ async def async_setup(hass: HomeAssistant, config_type: ConfigType) -> bool:
             webcomponent_name="home_assistant_eltako_frontend",
             sidebar_title="eltako",
             sidebar_icon="mdi:bus-electric",
-            module_url="home_assistant_eltako_frontend/static/static/index.html",
+            module_url="/static/static/index.html",
             embed_iframe=True,
             require_admin=True,
         )
