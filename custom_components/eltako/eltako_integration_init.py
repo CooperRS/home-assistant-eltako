c97332684d6593456434d8152511e7c9fbcfde12
Philipp Grimm <grimm.philipp@gmail.com>
2025-03-10 15:55:05 +0100

    added repeater mode as drop down

--- custom_components/eltako/eltako_integration_init.py
+++ custom_components/eltako/eltako_integration_init.py
@@ -111,7 +111,7 @@ async def async_setup(hass: HomeAssistant, config_type: ConfigType) -> bool:
             webcomponent_name="eltako",
             sidebar_title="eltako",
             sidebar_icon="mdi:bus-electric",
-            module_url="/eltako/index.html",
+            module_url="/eltako",
             embed_iframe=False,
             require_admin=False,
         )
