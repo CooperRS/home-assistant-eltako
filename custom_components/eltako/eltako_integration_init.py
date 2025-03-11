aceceab25d8a5fdd15e2d739a64542d14b8b4558
Philipp Grimm <grimm.philipp@gmail.com>
2025-03-11 11:45:34 +0100

    test frontend lib

--- custom_components/eltako/eltako_integration_init.py
+++ custom_components/eltako/eltako_integration_init.py
@@ -119,10 +119,10 @@ async def async_setup(hass: HomeAssistant, config_type: ConfigType) -> bool:
             webcomponent_name="home-assistant-eltako-frontend",
             sidebar_title="eltako",
             sidebar_icon="mdi:bus-electric",
-            module_url="eltako/index.html",
+            module_url="/eltako/index.html",
             embed_iframe=True,
             require_admin=True,
-            config_panel_domain=DOMAIN,
+            # config_panel_domain=DOMAIN,
         )
 
     LOGGER.info(f"[{LOG_PREFIX_INIT}] Eltako Integration initiallized. ... loading device configuration")
